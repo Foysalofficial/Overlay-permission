24c8f557cb91b9112c442ec20e911625df6fe0c4
foysalofficial <32913064git>
2022-03-22 16:29:31 +0600

    Overlaypermission

---
+++ app/src/main/java/com/foysaldev/overlaypermission/MainActivity.java
@@ -0,0 +1,26 @@
+package com.foysaldev.overlaypermission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(MainActivity.this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+            }
+        }
+    }
+}
